45edd48303349fa4b3fc4cf40ef62aabc0132c48
Alejandro <gomez@physics.rutgers.edu>
2016-01-26 17:41:28 +0100

    adding diff cuts

--- RUNAnalysis/python/cuts.py
+++ RUNAnalysis/python/cuts.py
@@ -7,10 +7,15 @@ selection[ 'Dibosons' ] = [
 		]
 
 selection[ 'RPVSt100' ] = [ 
-		[ 'massAsym', 0.30 ], [ 'deltaEtaDijet', 0.7 ] , [ 'jet1Tau21', 0.5 ], [ 'jet2Tau21', 0.5 ], [ 'jet1Tau31', 0.3 ] #, [ 'jet2Tau31', 0.35 ] ]
+		#[ 'massAsym', 0.30 ], [ 'deltaEtaDijet', 0.7 ] , [ 'jet1Tau21', 0.5 ], [ 'jet2Tau21', 0.5 ], [ 'jet1Tau31', 0.3 ] #v0
+		#[ 'massAsym', 0.30 ], [ 'deltaEtaDijet', 0.7 ] , [ 'jet1Tau21', 0.5 ], [ 'jet2Tau21', 0.5 ], [ 'jet1Tau31', 0.3 ], [ 'jet2Tau31', 0.35 ] #v1
+		#[ 'massAsym', 0.30 ], [ 'deltaEtaDijet', 0.7 ] , [ 'jet1Tau21', 0.5 ], [ 'jet2Tau21', 0.5 ] #v2
+		#[ 'massAsym', 0.30 ], [ 'deltaEtaDijet', 0.7 ], [ 'jet2Tau21', 0.5 ], [ 'jet2Tau31', 0.35 ]  #v3
+		#[ 'massAsym', 0.30 ], [ 'deltaEtaDijet', 0.7 ] , [ 'jet1Tau21', 1. ], [ 'jet2Tau21', 0.5 ], [ 'jet1Tau31', 1. ], [ 'jet2Tau31', 0.35 ] #v3p1
 		]
 
 selection[ 'RPVSt200' ] = [ 
-		[ 'massAsym', 0.10 ], [ 'deltaEtaDijet', 0.9 ], [ 'jet1Tau21', 0.4 ], [ 'jet2Tau21', 0.4 ] 
+		#[ 'massAsym', 0.10 ], [ 'deltaEtaDijet', 0.9 ], [ 'jet1Tau21', 0.4 ], [ 'jet2Tau21', 0.4 ] 
+		[ 'massAsym', 0.30 ], [ 'deltaEtaDijet', 0.7 ] , [ 'jet1Tau21', 0.5 ], [ 'jet2Tau21', 0.5 ], [ 'jet1Tau31', 0.3 ], [ 'jet2Tau31', 0.35 ] 
 		]
 		
